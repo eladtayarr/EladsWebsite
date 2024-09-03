faa16499d602af44976de51d2dfcc7603fed46d2
Elad Tayar <eladt1010@gmail.com>
2024-09-03 13:44:38 +0300

    removed index.html file in public

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5c2c43c7112f3823"; }
+/******/ 		__webpack_require__.h = function() { return "ead45afe63efd6b9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
