88345fcc87ec7b81d0e070b52b93db3ae75ca19c
Elad Tayar <eladt1010@gmail.com>
2024-09-06 11:35:15 +0300

    main

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "655307d64eff7285"; }
+/******/ 		__webpack_require__.h = function() { return "dc18a88578fd9532"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
